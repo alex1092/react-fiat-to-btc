90818f86ce18b8d5d82559af28b86354f8f96181
Alex <alittle1092@gmail.com>
2020-06-30 12:32:39 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import currencies from './supported-currencies.json'
 
 const App = () => {
+  const defaultCurrency = "AUD"
+  const bitcoinApi = "https://api.coindesk.com/v1/bpi/historical/close.json"
+  // [getter, setter] = useState(initialValue)
+  const [currency, setCurrency] = useState(defaultCurrency)
+  const [bitcoinData, setBitcoinData] = useState({})
+
+  // componentDidMount + if currency is updated
+  useEffect(() => {
+    console.log('inside useEffect')
+    fetch(`${bitcoinApi}?currency=${currency}`)
+      .then(res => res.json())
+      .then(data => setBitcoinData(data.bpi))
+  }, [currency])
+
   return (
-    <div >
-          The basic empty template
+    <div>
+      <span>Select your currency: </span>
+      <select value={currency} onChange={event => setCurrency(event.target.value)}>
+        {
+          currencies.map((item, index) => {
+            return <option key={index} value={item.currency}>{item.country}</option>
+          })
+        }
+      </select>
+
+      <h1>Bitcoin Data for {currency}</h1>
+      {
+        Object.keys(bitcoinData).map((date) =>
+          <div key={date}>Date: {date} Value: {bitcoinData[date]}</div>
+        )
+      }
     </div>
   )
 }
